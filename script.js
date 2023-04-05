0b10b6b085ed580b67c47aecee3b8341f8f1c4eb
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-04-05 01:15:02 -0300

    Projeto 02 EBAC - Concluido

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('formulario');
+const arrayName = [];
+const arraytele = [];
 var noteCompleta = '';
 
 form.addEventListener('submit', function(evento){
@@ -10,16 +12,22 @@ form.addEventListener('submit', function(evento){
 function addNumero(){
    const nome = document.getElementById('nome');
    const telefone = document.getElementById('telefone');
+   
+   if(arraytele.includes(telefone.value)){
+      alert(`Número ${telefone.value} já foi cadastrado`)
+   }else{
+      arrayName.push(nome.value)
+      arraytele.push(telefone.value);
 
-   let note = '<tr>';
-      note += `<td> ${nome.value}</td>`;
-      note += `<td> ${telefone.value}</td>`;
-      note += '</tr>';
+      let note = '<tr>';
+         note += `<td> ${nome.value}</td>`;
+         note += `<td> ${telefone.value}</td>`;
+         note += '</tr>';
 
-   noteCompleta += note;
-
-   nome.value= '';
-   telefone.value='';
+      noteCompleta += note;
+   }
+      nome.value= '';
+      telefone.value='';
 }
 
 function atualizaAgenda(){
